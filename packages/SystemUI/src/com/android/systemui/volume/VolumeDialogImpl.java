d4ec70252a1f5b6b7e99f289183fc13a56a57f3d
Pranav Vashi <neobuddy89@gmail.com>
2024-04-14 21:54:12 +0530

    SystemUI: VolumeDialog: Add content observer for panel timeout
    Fixes: https://github.com/crdroidandroid/issue_tracker/issues/371
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -371,7 +371,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
     private @DevicePostureController.DevicePostureInt int mDevicePosture;
     private int mOrientation;
     private final Lazy<SecureSettings> mSecureSettings;
-    private int mDialogTimeoutMillis;
+    private int mDialogTimeoutMillis = DIALOG_TIMEOUT_MILLIS;
     private final VibratorHelper mVibratorHelper;
     private final com.android.systemui.util.time.SystemClock mSystemClock;
     private final VolumePanelFlag mVolumePanelFlag;
@@ -431,7 +431,6 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         mVolumePanelNavigationInteractor = volumePanelNavigationInteractor;
         mVolumeNavigator = volumeNavigator;
         mSecureSettings = secureSettings;
-        mDialogTimeoutMillis = DIALOG_TIMEOUT_MILLIS;
         mVolumePanelFlag = volumePanelFlag;
         mInteractor = interactor;
 
@@ -467,6 +466,19 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                     false, volumePanelOnLeftObserver);
             volumePanelOnLeftObserver.onChange(true);
         }
+
+        ContentObserver volumeTimeoutObserver = new ContentObserver(null) {
+            @Override
+            public void onChange(boolean selfChange) {
+                mDialogTimeoutMillis = mSecureSettings.get().getIntForUser(
+                        Settings.Secure.VOLUME_DIALOG_DISMISS_TIMEOUT,
+                        DIALOG_TIMEOUT_MILLIS, UserHandle.USER_CURRENT);
+            }
+        };
+        mContext.getContentResolver().registerContentObserver(
+                Settings.Secure.getUriFor(Settings.Secure.VOLUME_DIALOG_DISMISS_TIMEOUT),
+                false, volumeTimeoutObserver);
+        volumeTimeoutObserver.onChange(true);
 
         initDimens();
 
@@ -686,8 +698,6 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         mDialogView.setAlpha(0);
         mDialogView.setLayoutDirection(
                 mVolumePanelOnLeft ? LAYOUT_DIRECTION_LTR : LAYOUT_DIRECTION_RTL);
-        mDialogTimeoutMillis = mSecureSettings.get().getInt(
-                Settings.Secure.VOLUME_DIALOG_DISMISS_TIMEOUT, DIALOG_TIMEOUT_MILLIS);
         mDialog.setCanceledOnTouchOutside(true);
         mDialog.setOnShowListener(dialog -> {
             mDialogView.getViewTreeObserver().addOnComputeInternalInsetsListener(this);
