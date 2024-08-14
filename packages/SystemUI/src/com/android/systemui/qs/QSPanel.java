64155295095f4cee39406552ae9f7463f2dd3a20
minaripenguin <minaripenguin@users.noreply.github.com>
2024-08-14 18:52:54 +0800

    SystemUI: Fix crash when switching QS view parents
    08-14 18:30:26.695  3143  3143 E AndroidRuntime: Process: com.android.systemui, PID: 3143
    08-14 18:30:26.695  3143  3143 E AndroidRuntime: java.lang.IndexOutOfBoundsException: index=2 count=1
    08-14 18:30:26.695  3143  3143 E AndroidRuntime: 	at android.view.ViewGroup.addInArray(ViewGroup.java:5429)
    08-14 18:30:26.695  3143  3143 E AndroidRuntime: 	at android.view.ViewGroup.addViewInner(ViewGroup.java:5322)
    08-14 18:30:26.695  3143  3143 E AndroidRuntime: 	at android.view.ViewGroup.addView(ViewGroup.java:5130)
    08-14 18:30:26.695  3143  3143 E AndroidRuntime: 	at android.view.ViewGroup.addView(ViewGroup.java:5070)
    08-14 18:30:26.695  3143  3143 E AndroidRuntime: 	at com.android.systemui.qs.QSPanel.switchToParent(QSPanel.java:935)
    08-14 18:30:26.695  3143  3143 E AndroidRuntime: 	at com.android.systemui.qs.QSPanel.switchToParent(QSPanel.java:596)
    08-14 18:30:26.695  3143  3143 E AndroidRuntime: 	at com.android.systemui.qs.QSPanel.switchAllContentToParent(QSPanel.java:584)
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -854,7 +854,8 @@ public class QSPanel extends LinearLayout {
             if (currentParent != null) {
                 currentParent.removeView(child);
             }
-            parent.addView(child, index);
+            int validIndex = Math.min(index, parent.getChildCount());
+            parent.addView(child, validIndex);
             return;
         }
         // Same parent, we are just changing indices
@@ -864,6 +865,7 @@ public class QSPanel extends LinearLayout {
             return;
         }
         parent.removeView(child);
-        parent.addView(child, index);
+        int validIndex = Math.min(index, parent.getChildCount());
+        parent.addView(child, validIndex);
     }
 }
