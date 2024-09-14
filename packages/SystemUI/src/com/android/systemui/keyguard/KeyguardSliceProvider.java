3995e3b2fe5fb4d47daf44b4339c54a5b529c5ab
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-09-14 08:22:36 +0530

    SystemUI: Move keyguard date slice row to a separate function
    Make it accessible to derived classes.
    
    Change-Id: I275475dcd825a3fe6917db73fd33d5c59a55caea

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
@@ -225,7 +225,7 @@ public class KeyguardSliceProvider extends SliceProvider implements
                 if (needsMediaLocked()) {
                     addMediaLocked(builder);
                 } else {
-                    builder.addRow(new RowBuilder(mDateUri).setTitle(mLastText));
+                    addDateLocked(builder);
                 }
                 addNextAlarmLocked(builder);
                 addZenModeLocked(builder);
@@ -238,6 +238,10 @@ public class KeyguardSliceProvider extends SliceProvider implements
             Trace.endSection();
         }
         return slice;
+    }
+
+    protected void addDateLocked(ListBuilder builder) {
+        builder.addRow(new RowBuilder(mDateUri).setTitle(mLastText));
     }
 
     protected boolean needsMediaLocked() {
