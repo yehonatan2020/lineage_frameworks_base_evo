23f567305ef8f21152eae91112ecc9305e3a68e0
minaripenguin <minaripenguin@users.noreply.github.com>
2024-12-07 11:09:46 +0800

    InternetDialogController: Allow scanning of networks while device is locked

--- packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogController.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogController.java
@@ -1125,7 +1125,7 @@ public class InternetDialogController implements AccessPointController.AccessPoi
     }
 
     public boolean isDeviceLocked() {
-        return !mKeyguardStateController.isUnlocked();
+        return false;
     }
 
     boolean activeNetworkIsCellular() {
