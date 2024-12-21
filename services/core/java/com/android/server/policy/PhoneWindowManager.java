LineageOS/android_frameworks_base

PhoneWindowManager: Only react to camera key when necessary
* With the camera already open, long pressing the camera key opens
  another instance of the cam app in front
* each of the newly started instances get added to the stack
* no need to pause the current one and bring another one to the front
  when we are already at the desired state

Change-Id: Ieaf50cc9bbec92da19787928b48d1f3806efb6b4

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -4425,8 +4425,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                         } else {
                             intent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
                         }
-                        isWakeKey = true;
-                        startActivityAsUser(intent, UserHandle.CURRENT_OR_SELF);
+                        if (!isAlreadyRunning(intent)) {
+                            isWakeKey = true;
+                            startActivityAsUser(intent, UserHandle.CURRENT_OR_SELF);
+                        }
                     }
                 }
                 break;
@@ -4691,6 +4693,38 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
 
         return result;
+    }
+
+    /**
+     * Check if an activity requested via an intent is already the current top / focused one
+     * @param intent
+     */
+    private boolean isAlreadyRunning(Intent intent) {
+        // When the screen is not on, we consider the activity to be not running
+        if (!isScreenOn()) {
+            return false;
+        }
+
+        ResolveInfo info = mPackageManager.resolveActivityAsUser(intent,
+                PackageManager.MATCH_DEFAULT_ONLY,
+                mCurrentUserId);
+        if (info == null || info.activityInfo == null || info.activityInfo.packageName == null) {
+            return false;
+        }
+
+        String resolvedName = info.activityInfo.packageName;
+
+        try {
+            final ActivityManager.StackInfo focusedStack =
+                    ActivityTaskManager.getService().getFocusedStackInfo();
+            if (focusedStack != null && focusedStack.topActivity != null) {
+                return resolvedName.equals(focusedStack.topActivity.getPackageName());
+            }
+        } catch (Exception e) {
+            // ignore
+        }
+
+        return false;
     }
 
     /**

yehonatan2020/lineage_frameworks_base_evo

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -5822,8 +5822,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                         } else {
                             intent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
                         }
-                        isWakeKey = true;
-                        startActivityAsUser(intent, UserHandle.CURRENT_OR_SELF);
+                        if (!isAlreadyRunning(intent)) {
+                            isWakeKey = true;
+                            startActivityAsUser(intent, UserHandle.CURRENT_OR_SELF);
+                        }
                     }
                 }
                 break;
@@ -6162,6 +6164,38 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mWindowWakeUpPolicy.wakeUpFromPowerKeyCameraGesture();
         }
         return true;
+    }
+
+    /**
+     * Check if an activity requested via an intent is already the current top / focused one
+     * @param intent
+     */
+    private boolean isAlreadyRunning(Intent intent) {
+        // When the screen is not on, we consider the activity to be not running
+        if (!isScreenOn()) {
+            return false;
+        }
+
+        ResolveInfo info = mPackageManager.resolveActivityAsUser(intent,
+                PackageManager.MATCH_DEFAULT_ONLY,
+                mCurrentUserId);
+        if (info == null || info.activityInfo == null || info.activityInfo.packageName == null) {
+            return false;
+        }
+
+        String resolvedName = info.activityInfo.packageName;
+
+        try {
+            final ActivityTaskManager.RootTaskInfo focusedStack =
+                    ActivityTaskManager.getService().getFocusedRootTaskInfo();
+            if (focusedStack != null && focusedStack.topActivity != null) {
+                return resolvedName.equals(focusedStack.topActivity.getPackageName());
+            }
+        } catch (Exception e) {
+            // ignore
+        }
+
+        return false;
     }
 
     /**