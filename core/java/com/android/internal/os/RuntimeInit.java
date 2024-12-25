sub77-bkp/frameworks_base

ActivityManagerNative: Prevent possible soft-reboot
E AndroidRuntime: java.lang.NullPointerException: Attempt to invoke interface method 'void android.app.IActivityManager.attachApplication(android.app.IApplicationThread)' on a null object reference
E AndroidRuntime: 	at android.app.ActivityThread.attach(ActivityThread.java:5110)
E AndroidRuntime: 	at android.app.ActivityThread.main(ActivityThread.java:5262)
E AndroidRuntime: 	at java.lang.reflect.Method.invoke(Native Method)
E AndroidRuntime: 	at java.lang.reflect.Method.invoke(Method.java:375)
E AndroidRuntime: 	at com.android.internal.os.ZygoteInit$MethodAndArgsCaller.run(ZygoteInit.java:904)
E AndroidRuntime: 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:699)
E AndroidRuntime: Error reporting crash
E AndroidRuntime: java.lang.NullPointerException: Attempt to invoke interface method 'void android.app.IActivityManager.handleApplicationCrash(android.os.IBinder, android.app.ApplicationErrorReport$CrashInfo)' on a null object reference
E AndroidRuntime: 	at com.android.internal.os.RuntimeInit$UncaughtHandler.uncaughtException(RuntimeInit.java:89)
E AndroidRuntime: 	at java.lang.ThreadGroup.uncaughtException(ThreadGroup.java:693)
E AndroidRuntime: 	at java.lang.ThreadGroup.uncaughtException(ThreadGroup.java:690)

Change-Id: I6940b723de455ae867ae784d4636e38a4196a689

core/java/com/android/internal/os/RuntimeInit.java
@@ -19,6 +19,7 @@ package com.android.internal.os;
 import android.app.ActivityManagerNative;
 import android.app.ActivityThread;
 import android.app.ApplicationErrorReport;
+import android.app.IActivityManager;
 import android.os.Build;
 import android.os.Debug;
 import android.os.IBinder;
@@ -86,8 +87,11 @@ public class RuntimeInit {
                 }
 
                 // Bring up crash dialog, wait for it to be dismissed
-                ActivityManagerNative.getDefault().handleApplicationCrash(
-                        mApplicationObject, new ApplicationErrorReport.CrashInfo(e));
+                final IActivityManager mgr = ActivityManagerNative.getDefault();
+                if (mgr != null) {
+                    mgr.handleApplicationCrash(
+                            mApplicationObject, new ApplicationErrorReport.CrashInfo(e));
+                }
             } catch (Throwable t2) {
                 try {
                     Clog_e(TAG, "Error reporting crash", t2);

yehonatan2020/lineage_frameworks_base_evo

Change-Id: I073fcb6d4e5ca506b7cfc953e1fc3667d84f5432
Signed-off-by: someone5678 <someone5678@users.noreply.github.com>

core/java/com/android/internal/os/RuntimeInit.java
@@ -167,8 +167,11 @@ public class RuntimeInit {
                 }
 
                 // Bring up crash dialog, wait for it to be dismissed
-                ActivityManager.getService().handleApplicationCrash(
-                        mApplicationObject, new ApplicationErrorReport.ParcelableCrashInfo(e));
+                final IActivityManager mgr = ActivityManager.getService();
+                if (mgr != null) {
+                    mgr.handleApplicationCrash(
+                            mApplicationObject, new ApplicationErrorReport.ParcelableCrashInfo(e));
+                }
             } catch (Throwable t2) {
                 if (t2 instanceof DeadObjectException) {
                     // System process is dead; ignore