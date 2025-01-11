PixelExperience/frameworks_base

Import CutoutUtils
Change-Id: I7b9514b11a0d3853b1aac0292b0a9c9bde8bf04e

core/java/com/android/internal/util/custom/cutout/CutoutUtils.java
@@ -1,0 +1,49 @@
+/*
+* Copyright (C) 2019 The Pixel Experience Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.android.internal.util.custom.cutout;
+
+import android.content.Context;
+
+public class CutoutUtils {
+    public static boolean hasCutout(Context context) {
+        return hasCutout(context, false);
+    }
+
+    public static boolean hasCutout(Context context, boolean ignoreCutoutMasked) {
+        boolean hasCutout = context.getResources().getBoolean(com.android.internal.R.bool.config_physicalDisplayCutout);
+        if (!ignoreCutoutMasked && hasCutout){
+            return !context.getResources().getBoolean(
+                com.android.internal.R.bool.config_maskMainBuiltInDisplayCutout);
+        }
+        return hasCutout;
+    }
+
+    public static boolean hasCenteredCutout(Context context) {
+        return hasCenteredCutout(context, false);
+    }
+
+    public static boolean hasCenteredCutout(Context context, boolean ignoreCutoutMasked) {
+        if (!hasCutout(context, ignoreCutoutMasked)){
+            return false;
+        }
+        boolean hasCenteredCutout = context.getResources().getBoolean(com.android.internal.R.bool.config_physicalDisplayCutoutCentered);
+        if (!ignoreCutoutMasked && hasCenteredCutout){
+            return !context.getResources().getBoolean(
+                com.android.internal.R.bool.config_maskMainBuiltInDisplayCutout);
+        }
+        return hasCenteredCutout;
+    }
+}

yehonatan2020/lineage_frameworks_base_evo

core/java/com/android/internal/util/evolution/cutout/CutoutUtils.java
@@ -1,0 +1,49 @@
+/*
+* Copyright (C) 2019 The Pixel Experience Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.android.internal.util.evolution.cutout;
+
+import android.content.Context;
+
+public class CutoutUtils {
+    public static boolean hasCutout(Context context) {
+        return hasCutout(context, false);
+    }
+
+    public static boolean hasCutout(Context context, boolean ignoreCutoutMasked) {
+        boolean hasCutout = context.getResources().getBoolean(com.android.internal.R.bool.config_physicalDisplayCutout);
+        if (!ignoreCutoutMasked && hasCutout){
+            return !context.getResources().getBoolean(
+                com.android.internal.R.bool.config_maskMainBuiltInDisplayCutout);
+        }
+        return hasCutout;
+    }
+
+    public static boolean hasCenteredCutout(Context context) {
+        return hasCenteredCutout(context, false);
+    }
+
+    public static boolean hasCenteredCutout(Context context, boolean ignoreCutoutMasked) {
+        if (!hasCutout(context, ignoreCutoutMasked)){
+            return false;
+        }
+        boolean hasCenteredCutout = context.getResources().getBoolean(com.android.internal.R.bool.config_physicalDisplayCutoutCentered);
+        if (!ignoreCutoutMasked && hasCenteredCutout){
+            return !context.getResources().getBoolean(
+                com.android.internal.R.bool.config_maskMainBuiltInDisplayCutout);
+        }
+        return hasCenteredCutout;
+    }
+}